74d7e5926d7a5ec281c5b8bfe6ed65919fa1c3b0
minhyeong <minhyeong.dev@gmail.com>
2025-05-17 06:15:06 +0900

    feat: boolean logic

--- src/utils.ts
+++ src/utils.ts
@@ -1,27 +1,52 @@
+import { ToRowOption } from "./interface";
+
+const IS_FIELD_REGEX = /^is_/;
 const toObject = <T>(keys: string[], row: Record<string, any>): T => {
-	const obj = {} as T;
-	Object.entries(keys).forEach(([key, column]) => {
-		(obj as any)[key] = row[column];
+	const snakeKeys = convertToSnakeStrings([...keys]);
+	const result = {} as Record<string, unknown>;
+	keys.forEach((key, i) => {
+		if(IS_FIELD_REGEX.test(snakeKeys[i])) result[key] = toBoolean(row[snakeKeys[i]]);
+		else result[key] = row[snakeKeys[i]];
 	});
-	return obj;
+	return result as T;
 };
-const toRow = <T>(keys: readonly string[], obj: T): Record<string, any> => {
+const toRow = <T>(keys: readonly string[], obj: T, autoSetKeys: readonly string[],option?: ToRowOption): Record<string, any> => {
+	const isAutoSet = option?.isAutoSet ?? true;
 	const row: Record<string, unknown> = {};
-	Object.entries(keys).forEach(([key, column]) => {
-		row[column] = obj[key as keyof T];
+	keys.forEach((key) => {
+		if (isAutoSet && autoSetKeys.includes(String(key))) row[[key][0]] = undefined;
+		else row[[key][0]] = obj[key as keyof typeof obj];
 	});
-	return row;
+	const snakeRow = Object.entries(row as Record<string, unknown>).reduce((acc, [key, value]) => {
+		acc[convertToSnakeString(key)] = value;
+		return acc;
+	}, {} as Record<string, unknown>);
+	return snakeRow;
 };
+
 const toPartialRow = <T>(keys: string[], obj: Partial<T>): Record<string, unknown> => {
 	const row: Record<string, unknown> = {};
 	Object.entries(obj).forEach(([key, value]) => {
 		if (keys.includes(key)) {
-			row[key] = value;
+			if (IS_FIELD_REGEX.test(key)) row[key] = toDbBoolean(value);
+			else row[key] = value;
 		}
 	});
 	return row;
 };
-export { toObject, toRow, toPartialRow }
+const toBoolean = (value: any): boolean => {
+	if (typeof value === 'boolean') return value;
+	if (value === 1 || value === '1') return true;
+	if (value === 0 || value === '0') return false;
+	return Boolean(value);
+};
+const toDbBoolean = (value: any): 1 | 0 => {
+	if (typeof value === 'boolean') return value ? 1 : 0;
+	if (value === 1 || value === '1') return 1;
+	if (value === 0 || value === '0') return 0;
+	return Boolean(value) ? 1 : 0;
+};
+export { toObject, toRow, toPartialRow, toBoolean, toDbBoolean }
 export function removeUndefined<T>(obj: T): T {
 	for (const key in obj) {
 		if (obj[key] === undefined) {
