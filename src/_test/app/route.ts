240e4756cc8aaf0bb7d55bdc837ee50e8a5aef39
minhyeong <minhyeong.dev@gmail.com>
2025-06-23 11:34:09 +0900

    fix: merge issue

--- src/_test/app/route.ts
+++ src/_test/app/route.ts
@@ -4,35 +4,32 @@ import novelService from "../service/novel";
 const defaultRouter = express.Router();
 
 defaultRouter.get("/", async (req, res) => {
-	try {
-		const novels = await novelService.read();
-		return res.json(novels);
-	}
-	catch (error) {
-		console.log(error)
-	}
+  try {
+    const novels = await novelService.read();
+    return res.json(novels);
+  } catch (error) {
+    console.log(error);
+  }
 });
 defaultRouter.post("/", async (req, res) => {
-	try {
-		const novels = await novelService.create(req.body)
-		return res.json(novels);
-	} catch (error) {
-		return res.status(500).json({ error });
-	}
-})
+  try {
+    const novels = await novelService.create(req.body);
+    return res.json(novels);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
 defaultRouter.patch("/", async (req, res) => {
-	try {
-		const novels = await novelService.update(10, req.body)
-		return res.json(novels);
-	}
-	catch (error) {
-		console.log(error)
-	}
-})
+  try {
+    const novels = await novelService.update(10, req.body);
+    return res.json(novels);
+  } catch (error) {
+    console.log(error);
+  }
+});
 defaultRouter.delete("/", async (req, res) => {
-	const novels = await novelService.delete(9)
-	return res.json(novels);
-})
-
+  const novels = await novelService.delete(9);
+  return res.json(novels);
+});
 
 export default defaultRouter;
